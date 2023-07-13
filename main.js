976a50daf9a3cab38cdd651b75b119d66a9c0e80
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-07-13 03:22:57 -0600

    dark-mode-practice

---
+++ main.js
@@ -0,0 +1,17 @@
+/* detectar el click del boton para cambiar a dark mode */
+/* 
+ TODO => EL DARK MODE CONSISTE SOLAMENTE EN LA CLASE DARK
+* 1. crear clase al body para dark mode
+* 2. dar estilos a los puntos pricipales con la clase dark
+* 3. remover la clase dark del body
+* 4. boton para escuchar el evento click y poder prender o apagar el evento
+* 5. buscar el boton por id o class en main.js
+* 6. agregar un eventListener al boton btn.addEventListener
+* 7. seleccionar a body dentro del documento
+* 8. exportar por default la funcion
+* 9. importar la funcion darkMode a main.js
+* 10. ejecutar la funcion dentro del main.js
+*/
+import darkMode from './components/darkMode.js'
+
+darkMode()
